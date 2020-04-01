b35cceabc7da604002c335870c902ff94131433c
lowie-einbinder <lowieeinbinder@gmail.com>
2020-03-31 21:58:28 -0400

    Making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,3 +2,11 @@ class PicturesController < ApplicationController
     def index
     end
 end
+
+class PicturesController < ApplicationController
+
+    def index
+      @pictures = Picture.all
+    end
+  
+  end
